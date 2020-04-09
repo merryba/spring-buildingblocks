87d6050616c73322d0edac8314f9b07ad3a79285
merryba@gmail.com <merryba@gmail.com>
2020-04-09 16:31:13 -0700

    first commit

---
+++ src/main/java/com/api/merryba/SpringBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.api.merryba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/merryba/SpringBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.merryba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
