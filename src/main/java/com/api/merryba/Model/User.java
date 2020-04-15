a19b1f70244fd1844b5ade02315657134834d6a1
merryba@gmail.com <merryba@gmail.com>
2020-04-15 12:57:15 -0700

    JsonIgnore First Commit

--- src/main/java/com/api/merryba/Model/User.java
+++ src/main/java/com/api/merryba/Model/User.java
@@ -11,8 +11,12 @@ import javax.persistence.Table;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name="user")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends RepresentationModel<User>{
 		@Id
 		@GeneratedValue
@@ -33,7 +37,8 @@ public class User extends RepresentationModel<User>{
 		@Column(name="ROLE",length=50,nullable=false)
 		private String role;
 		
-		@Column(name="SSN",length=50,nullable=false,unique=true)
+		@JsonIgnore
+		@Column(name="SSN",length=50,nullable=true,unique=true)
 		private String ssn;
 		
 		@OneToMany(mappedBy="user")
