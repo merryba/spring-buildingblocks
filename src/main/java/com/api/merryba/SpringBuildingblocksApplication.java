b590467282c80776e77d3e898723c3c15a0bc931
merryba@gmail.com <merryba@gmail.com>
2020-04-15 12:25:50 -0700

    First Commit I18N

--- src/main/java/com/api/merryba/SpringBuildingblocksApplication.java
+++ src/main/java/com/api/merryba/SpringBuildingblocksApplication.java
@@ -1,13 +1,34 @@
 package com.api.merryba;
 
+import java.util.Locale;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 //Hello  World 
 @SpringBootApplication
 public class SpringBuildingblocksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBuildingblocksApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");;
+		return messageSource;
 	}
 
 }
